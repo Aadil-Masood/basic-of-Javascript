00e8e7fbe7ad8331e3c77e4d72f16b9054b0f1a2
Aadil-Masood <aadilsiddiqui1995@gmail.com>
2021-07-24 21:01:25 +0530

    basic commit

---
+++ index.js
@@ -0,0 +1,77 @@
+             JavaScript
+//JavaScript is the world’s most popular programming language.
+//JavaScript was initially created to “make web pages alive”.
+//JavaScript is a lightweight, interpreted programming language.
+//JavaScript is dynamically-typed language, i.e. it performs type checking at runtime. This means that scripts written in dynamically-typed languages can compile even if they contain errors that will prevent the script from running properly (if at all).
+//It is a full-fledged programming language that enables dynamic interactivity on websites when applied to an HTML document.
+//JavaScript ≠ Java 
+
+Linking JavaScript to HTML document:
+    // 1.Internally
+   // 2.Externally
+
+
+   for Eg .
+   1.Internally
+
+//    <html>
+//     <head>
+//       <title>JavaScript</title>
+//         <script>
+//           <!--
+//             JavaScript Code......!
+//            -->
+//         </script> 
+//    </head>
+//   <body>         
+//   </body>
+// </html>
+ 
+2.Externally
+   
+{/* <html>
+<head>
+  <title>JavaScript</title>
+  
+  <script src="myScript.js"></script>
+</head>
+<body>   
+         
+</body>
+</html> */}
+
+
+Datatypes in JavaScript
+  1. Primitive
+  2. Refferential  
+
+
+ Primitive are:-
+
+//primitive data type
+//1.number
+//2.string
+//3.undefined
+//4.null
+//5.boolean
+//6.symbol
+
+var a=6;
+var b="Aadil Masood";
+var c;
+var d=null;
+var e=true;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+
+console.log(c);//hosting
+var c;
